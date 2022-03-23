0eb1e25ca2f69c35d75382f6a22be1845b9e2f87
fcaihjuh <florence0cai@gmail.com>
2022-03-23 10:27:00 +0100

    update

--- backend1/src/app/routes/routes.php
+++ backend1/src/app/routes/routes.php
@@ -1,7 +1,9 @@
 <?php
 
 
-use \reu\back1\app\controller\EventController;
+use \reu\back1\app\controller\eventController;
+use \reu\back1\app\controller\memberController;
+use \reu\back1\app\controller\commentController;
 use \reu\back1\app\middleware\Middleware;
 use \reu\back1\app\middleware\EventValidator as EventValidator;
 use \DavidePastore\Slim\Validation\Validation as Validation ;
@@ -11,29 +13,50 @@ $validators = EventValidator::create_validators();
 //Routes de l'API
 
 //Route pour retourner le contenu d'un événement
-$app->get('/events/{id}[/]', EventController::class. ':OneEvent')->setName('oneEvent')->add(middleware::class. ':putIntoJson');
+$app->get('/events/{id}[/]', eventController::class. ':getOneEvent')->setName('oneEvent');
 
 
 //Route pour retourner le contenu de tous les événements
-$app->get('/events[/]', EventController::class. ':getAllEvents')->setName('getAllEvents')->add(middleware::class. ':putIntoJson');
+$app->get('/events[/]', eventController::class. ':getAllEvents')->setName('allEvents');
+
+
+//Route pour créer un événement 
+//$app->post('/events/create[/]', eventController::class. ':createEvent')->setName('createEvent');
 
 
 //Route pour modifier le contenu d'un événement
-$app->put('/events/{id}[/]', EventController::class. ':putEvent')->setName('putEvent')->add(middleware::class. ':putIntoJson');
+$app->put('/events/{id}[/]', eventController::class. ':putEvent')->setName('putEvent');
 
 
 //Route pour les participants d'un événement
-$app->get('/events/{id}/participants', EventController::class.':getAllUser')->setName('allUser')->add(middleware::class. ':putIntoJson');
+$app->get('/events/{id}/members', eventController::class.':getAllUsers')->setName('allUser');
 
-//Route pour un user
-$app->get('/events/{id}/participant', EventController::class.':OneUser')->setName('OneUser')->add(middleware::class. ':putIntoJson');
+
+//Route pour un participant
+$app->get('/events/{id}/member', memberController::class.':getOneUser')->setName('oneUser');
 
 
 //Route pour tous les commentaires
-$app->get('/comments[/]', EventController::class.':getAllComment')->setName('allComment')->add(middleware::class. ':putIntoJson');
+$app->get('/comments[/]', commentController::class.':getAllComments')->setName('allComment');
 
-///Route pour retourner le contenu d'un commentaire
-$app->get('/comments/{id}[/]', EventController::class. ':OneComment')->setName('oneComment')->add(middleware::class. ':putIntoJson');
+
+//Route pour retourner le contenu d'un commentaire
+$app->get('/comments/{id}[/]', eventController::class. ':getOneComment')->setName('oneComment');
+
+
+//Route pour l'inscription
+$app->post('/signup[/]', memberController::class. ':signUp')->setName('signUp');
+
+
+//Route pour la connexion
+$app->post('/members/signin[/]', memberController::class. ':signIn')->setName('signIn');
+
+
+//Route pour la déconnexion
+//$app->delete('/members/signout[/]', memberController::class.':signOut')->setName('signOut');
+
+
+
 
 
 ?>
